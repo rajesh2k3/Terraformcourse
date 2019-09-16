e4e61d77655a2df9a1c1219be49d7e707d37d1a3
rajesh2k3 <sraju2k3@gmail.com>
2019-09-16 21:59:41 +0000

    Updated the Access & Secret Keys values

--- Section7_Jobcasestudy#1_Webapp/hostingwebsite/vpc/provider.tf
+++ Section7_Jobcasestudy#1_Webapp/hostingwebsite/vpc/provider.tf
@@ -1,7 +1,12 @@
 # Define the provider
+# access_key = "${var.AWS_ACCESS_KEY}" # AWS Access key
+# secret_key = "${var.AWS_SECRET_KEY}" # AWS Secret Access Key
+# region = "${var.AWS_REGION}"  # The region in which you want to Operate
+
 provider "aws"
 {
-  access_key = "${var.AWS_ACCESS_KEY}" # AWS Access key
-  secret_key = "${var.AWS_SECRET_KEY}" # AWS Secret Access Key
-  region = "${var.AWS_REGION}"  # The region in which you want to Operate
+access_key = "${var.AWS_ACCESS_KEY}"
+secret_key = "${var.AWS_SECRET_KEY}"
+region = "${var.AWS_REGION}"
 }
+
