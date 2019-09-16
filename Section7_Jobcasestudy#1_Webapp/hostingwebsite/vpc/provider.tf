68a9b388d9a90dd71b08b4ed8f11024854a971a5
rajesh2k3 <sraju2k3@gmail.com>
2019-09-16 22:13:18 +0000

    Updated the code

--- Section7_Jobcasestudy#1_Webapp/hostingwebsite/vpc/provider.tf
+++ Section7_Jobcasestudy#1_Webapp/hostingwebsite/vpc/provider.tf
@@ -1,12 +1,9 @@
 # Define the provider
-# access_key = "${var.AWS_ACCESS_KEY}" # AWS Access key
-# secret_key = "${var.AWS_SECRET_KEY}" # AWS Secret Access Key
-# region = "${var.AWS_REGION}"  # The region in which you want to Operate
 
 provider "aws"
 {
-access_key = "${var.AWS_ACCESS_KEY}"
-secret_key = "${var.AWS_SECRET_KEY}"
-region = "${var.AWS_REGION}"
+access_key = "${var.AWS_ACCESS_KEY}" # AWS Access key
+secret_key = "${var.AWS_SECRET_KEY}" # AWS Secret Access Key
+region = "${var.AWS_REGION}"  # The region in which you want to Operate
 }
 
